41f249fdfa6608956404ce6552e9dbba92aff1ef
Kevin Sierras <70074691+kevsierras@users.noreply.github.com>
2022-11-11 11:29:37 +0000

    Added pieces on top of the board. Pieces are a little too big at the moment.

--- chessEngine.py
+++ chessEngine.py
@@ -7,18 +7,18 @@ class GameState():
     def __init__(self):
         # board is in characters to for easy parsing
         # 8x8 list, each list has 2 characters
-        # The 'b' and 'w' represent the color
-        # The 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' represent the pieces
-        # The '--' represent an empty space
+        # The lowercase represents the black pieces, uppercase are the white pieces
+        # The 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' represent the individual pieces
+        # The '-' represent an empty space
         self.board = [
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+            ["r", "n", "b", "q", "k", "b", "n", "r"],
+            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["P", "P", "P", "P", "P", "P", "P", "P"],
+            ["R", "N", "B", "Q", "K", "B", "N", "R"]
         ]
         self.whiteToMove = True
         self.moveLog = []
--- main.py
+++ main.py
@@ -4,32 +4,24 @@ This is our main driver file. It will be responsible for handling user input and
 import pygame as py
 import chessEngine
 
-# WIDTH and HEIGHT are 512 to evenly show all the squares
-WIDTH = 512
-HEIGHT = 512
+# WIDTH and HEIGHT can be adjusted for testing purpose
+WIDTH = 400
+HEIGHT = 400
 DIMENSION = 8 # dimension of the chess board
-SQ_SIZE = HEIGHT // DIMENSION
-
-
-'''
-Load images of chess pieces
-'''
-def loadImages():
-    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bq', 'bR', 'bN', 'bB', 'bK', 'bQ']
-    # load pieces here
-    # use for loop to load images onto pieces
+SQ_SIZE = HEIGHT / DIMENSION
+IMAGES = {}
 
 '''
 The main driver for our code. This will handle user input
 '''
-def main():
+def startGame(): 
     py.init()
     screen = py.display.set_mode((WIDTH, HEIGHT))
     # can add user interface with commands or a side interface to show moves (optional)
     clock = py.time.Clock()
     screen.fill(py.Color("white"))
     gameState = chessEngine.GameState()
-    # loadImages() # only do this once, before the while loop
+    loadImages() # only do this once, before the while loop
     running = True
     while running:
         for e in py.event.get():
@@ -41,20 +33,30 @@ def main():
         py.display.flip()
 
 '''
-Responsible for the graphics of the current game state
+Load images of chess pieces
+'''
+def loadImages():
+    pieces = ['p', 'r', 'n', 'b', 'q', 'k', 'P', 'R', 'N', 'B', 'Q', 'K']
+    # load pieces here
+    for piece in pieces: 
+      IMAGES[piece] = py.transform.scale(py.image.load("chess_pieces/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    
+
+'''
+Graphics driver
 '''
 def drawGameState(screen, gameState):
     drawBoard(screen) # draw squares on the drawBoard
 
     # add in piece highlighting or move suggestions (later)
 
-    # drawPieces(screen, gameState.board) # draw pieces on the squares
+    drawPieces(screen, gameState.board)
 
 '''
 Draw the squares on the board. Top left square is always white.
 '''
 def drawBoard(screen):
-    colors = [py.Color("white"), py.Color("gray")]
+    colors = [py.Color("gray"), py.Color("black")]
     for row in range(DIMENSION):
         for column in range(DIMENSION):
             color = colors[((row+column) % 2)]
@@ -64,8 +66,15 @@ def drawBoard(screen):
 Draw the chess pieces on the board using current GameState.board
 '''
 def drawPieces(screen, board):
-    pass
+    for row in range(DIMENSION): 
+      for column in range(DIMENSION): 
+        piece = board[row][column]
+        # not an empty space
+        if piece != '-':
+          screen.blit(IMAGES[piece], py.Rect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
+def main():
+    startGame()
 
 if __name__ == "__main__":
     main()
